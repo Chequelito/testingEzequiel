03525813be0a6cee5a0cf6c6e3b8945a0d091ac2
Ezequiel Herrera <thebearginger@gmail.com>
2021-05-20 04:16:54 -0600

    idk

--- src/template.js
+++ src/template.js
@@ -1,21 +1,42 @@
 function generateManager(manager){
-    return ` <div><h1>${manager.getName()}</h1>
-    <h3>${manager.getId()}</h3>
-    ${manager.getEmail()}
-    <h3>${manager.getRole()}</div></div>`
+    return `
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${manager.getName()}/h5>
+    <p class="card-text">${manager.getRole()}</p>
+  </div>  
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${manager.getId()}</li>
+    <li class="list-group-item">${manager.getEmail()}</li>
+    <li class="list-group-item"></li>
+  </ul></div>`
 
 }
 function generateEngineer(engineer){
-return ` <div><h1>${engineer.getName()}</h1>
-    <h3>${engineer.getId()}</h3>
-    ${engineer.getEmail()}
-    <h3>${engineer.getRole()}</div></div>`
+return `
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${engineer.getName()}</h5>
+    <p class="card-text">${engineer.getRole()}</p>
+  </div>  
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${engineer.getId()}</li>
+    <li class="list-group-item">${engineer.getEmail()}</li>
+    <li class="list-group-item"></li>
+  </ul></div>`
 }
 function generateIntern(intern){
-    return ` <div><h1>${intern.getName()}</h1>
-    <h3>${intern.getId()}</h3>
-    ${intern.getEmail()}
-    <h3>${intern.getRole()}</div></div>`
+    return `
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${intern.getName()}</h5>
+    <p class="card-text">${intern.getRole()}</p>
+  </div>  
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${intern.getId()}</li>
+    <li class="list-group-item">${intern.getEmail()}</li>
+    <li class="list-group-item">${intern.getSchool}</li>
+  </ul></div>`
 
 }
 function generateTeam(data){
@@ -31,44 +52,31 @@ for (i=0; i<data.length; i++) {
         teamCards.push(generateEngineer(data[i]))
 
     }
-    return teamCards.join("")
+    return teamCards.join(" ")
 }
+return generateTeam;
 }
+
 function generatePage(data){
     return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
-        crossorigin="anonymous"
-      />
-      <link rel="stylesheet" href="./style.css" />
-      <title>Employee Profiles</title>
-    </head>
-    <body>
-      <header>
-        <nav class="navbar navbar-dark bg-dark">
-          <div class="container-fluid d-flex justify-content-center">
-            <span class="navbar-brand m-5 h1">My Team</span>
-          </div>
-        </nav>
-      </header>
-      <main>
-      ${generateTeam(data)}
-      </main>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-      crossorigin="anonymous"
-    ></script>
-    <script src="../index.js"></script>
-  </body>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<header><div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">My Team</h1>
+    
+  </div>
+</div></header>
+<body>
+  ${generateTeam(data)}
+</body>
 </html>`
 }
 
-module.exports = generatePage
+module.exports = generatePage;
